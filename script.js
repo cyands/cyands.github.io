bbe238f441a30f702a57115fa4430ded5e27fdb6
cyands <cyands@gmail.com>
2026-07-23 15:22:52 +0900

    Deploy initial professional site

---
+++ script.js
@@ -0,0 +1,11 @@
+(() => {
+  const toggle = document.querySelector('.menu-toggle');
+  const navigation = document.querySelector('#site-nav');
+
+  if (!toggle || !navigation) return;
+
+  toggle.addEventListener('click', () => {
+    const isOpen = navigation.classList.toggle('is-open');
+    toggle.setAttribute('aria-expanded', String(isOpen));
+  });
+})();
